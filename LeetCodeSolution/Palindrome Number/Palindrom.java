4412c8851256111b28d394b311297a84ee33f664
Wiritipon Thapkhan <81100736+zignalssss@users.noreply.github.com>
2022-12-05 00:54:42 +0700

    Update Palindrom.java

--- LeetCodeSolution/Palindrome Number/Palindrom.java
+++ LeetCodeSolution/Palindrome Number/Palindrom.java
@@ -25,20 +25,24 @@
  */
 class Palindrome {
     public boolean isPalindrome(int x) {
-       boolean r = true;
-        String noReve = Integer.toString(x);
-        String reve = "";
-        for(int i = noReve.length()-1;i>=0;i--){
-             reve += noReve.charAt(i);
+            char ch;
+        if(x < 0){
+            return false;
+        }else{
+            String str = Integer.toString(x);
+            System.out.println(str);
+            String reverse = "";
+            for(int i = 0;i<str.length();i++){
+                ch = str.charAt(i);
+                reverse = ch+reverse;
+            }
+            if(str.equals(reverse)){
+                return true;
+            }else{
+                return false; 
+            }
+
         }
-        for(int j = 0;j<noReve.length();j++){
-              char a = noReve.charAt(j);
-              char b = reve.charAt(j);
-              if(a!=b){
-                  r = false;
-                  break;
-              }
-        }
-        return r;
+
     }
 }
